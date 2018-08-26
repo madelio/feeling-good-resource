2166daf3e4b92071f78ed9719317421b8842ec21
madel <madel.asistio@gmail.com>
2018-08-26 10:30:57 -0700

    built project

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,2 @@
-ember build --environment production
-git checkout --orphan gh-pages
-robocopy dist/ ./
-robocopy dist/assets ./assets
-git add index.html assets/
-git commit -m 'deployed pages'
-git push
-git checkout master
+REM ember build --environment production
+git checkout gh-pages && robocopy dist/ ./ && robocopy dist/assets ./assets && git add -A & git commit -m "deployed pages" && git push && git checkout master
